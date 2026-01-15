22b14268e789eac50bed22028d62c5acc871f619
TukS <91913765+gustitriprayoga@users.noreply.github.com>
2026-01-16 01:00:07 +0700

    ss

--- logger-v1.lua
+++ logger-v1.lua
@@ -1,8 +1,7 @@
-local WindUI = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/gustitriprayoga/contoh-proyek/refs/heads/main/wind-ui.lua"))()
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/gustitriprayoga/contoh-proyek/refs/heads/main/wind-ui.lua"))()
 
-local Window = WindUI:CreateWindow({
-    Title = "My Super Hub",
-    Icon = "door-open", -- lucide icon. optional
-    Author = "by .ftgs and .ftgs" -- optional
+local Window = WindUI:Window({
+    Title = "10s Dev |",
+    Footer = "made by @G",
+    Version = 1
 })
